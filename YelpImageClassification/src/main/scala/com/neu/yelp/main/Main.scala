7290e7d2f56b32d4817b30e0591ea4b45cca093d
kunaldeora <deora.k@husky.neu.edu>
2017-04-11 02:40:03 -0400

    Modification in Model Labels v2

--- YelpImageClassification/src/main/scala/com/neu/yelp/main/Main.scala
+++ YelpImageClassification/src/main/scala/com/neu/yelp/main/Main.scala
@@ -23,7 +23,7 @@ object Main{
 
     val img2DataMap = ImageUtils.img2Map("C:\\BigDataScalaProject\\Yelp Image Classification Model\\Dataset\\train_photos\\train_photos\\train_photos",image2BizMap);
 
-    println("Images Data : " + img2DataMap.size)
+    println("image2BizMap : " + img2DataMap.size)
 
     val transformedData = new TransformData(img2DataMap,image2BizMap,biz2LabelMap,"train")
 
